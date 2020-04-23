b2e862727f96ad22db6ffe271e38c64433d91d60
Narendranath <naren.7090@outlook.com>
2020-04-23 21:28:31 +0530

    wip

--- WebapiAngular/LSDataApi/LSDataApi/Startup.cs
+++ WebapiAngular/LSDataApi/LSDataApi/Startup.cs
@@ -112,7 +112,7 @@ namespace LSDataApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            // loggerFactory.AddFile("Logs/DataApi-{Date}.txt");
+            loggerFactory.AddFile("Logs/DataApi-{Date}.txt");
             if (env.IsDevelopment())
             {
                 // _logger.LogInformation("In Development environment");
