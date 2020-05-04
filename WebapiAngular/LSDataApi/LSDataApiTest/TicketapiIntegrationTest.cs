6342ff6cc0a6016b5e759f220ec2deea8c4df5e2
Narendranath <naren.7090@outlook.com>
2020-05-04 17:02:29 +0530

    added integration test project
    added integration test project

---
+++ WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
@@ -0,0 +1,30 @@
+using LSDataApi;
+using LSDataApi.Controllers;
+using LSDataApi.Tests;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LSDataApiTest
+{
+    public class TicketapiIntegrationTest : IClassFixture<TestFixture<Startup>>
+    {
+        private HttpClient Client;
+
+        public TicketapiIntegrationTest(TestFixture<Startup> fixture)
+
+        {
+            Client = fixture.Client;
+        }
+
+        [Fact]
+        public async Task GetApplicationMasterPassTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
