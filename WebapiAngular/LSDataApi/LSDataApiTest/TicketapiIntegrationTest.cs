311cccf79a4491e9eeb23637a8fb27302261066d
Narendranath <naren.7090@outlook.com>
2020-05-06 16:45:00 +0530

    Update TicketapiIntegrationTest.cs

--- WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
+++ WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
@@ -11,41 +11,14 @@ using Xunit;
 
 namespace LSDataApiTest
 {
-    public class TicketapiIntegrationTest// : IClassFixture<TestFixture<Startup>>
+    public class TicketapiIntegrationTest : IClassFixture<TestFixture<Startup>>
     {
         private HttpClient Client;
-        /*
+
         public TicketapiIntegrationTest(TestFixture<Startup> fixture)
 
         {
             Client = fixture.Client;
-        }
-*/
-
-        public TicketapiIntegrationTest()
-        {
-            Client = new HttpClient();
-            Client.BaseAddress = new Uri("http://localhost/webapp");
-            Client.DefaultRequestHeaders.Accept.Clear();
-            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            JObject obj = JObject.FromObject(new
-            {
-                username = "naren.7090@gmail.com",
-                password = "1234"
-            });
-
-            var body = obj.ToString();
-            HttpContent content = new StringContent(body, Encoding.UTF8, "application/json");
-            var responsetoken = Client.PostAsync("api/userapi/authenticate", content).Result;
-            string authtoken = "";
-            if (responsetoken != null)
-            {
-                var jsonString = responsetoken.Content.ReadAsStringAsync().Result;
-                JObject objrespose = JObject.Parse(jsonString);
-                authtoken = (string)objrespose["Token"];
-            }
-            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authtoken);
         }
 
         [Fact]
