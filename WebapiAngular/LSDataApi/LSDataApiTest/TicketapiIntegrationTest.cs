1449ed0b3c517cc723d58f31d43131fe71ae456c
Narendranath <naren.7090@outlook.com>
2020-05-06 14:10:15 +0530

    wip changes related to Integration testing in CI pipeline
    wip changes related to Integration testing in CI pipeline

--- WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
+++ WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
@@ -1,21 +1,51 @@
 using LSDataApi;
 using LSDataApi.Controllers;
 using LSDataApi.Tests;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace LSDataApiTest
 {
-    public class TicketapiIntegrationTest : IClassFixture<TestFixture<Startup>>
+    public class TicketapiIntegrationTest// : IClassFixture<TestFixture<Startup>>
     {
         private HttpClient Client;
-
+        /*
         public TicketapiIntegrationTest(TestFixture<Startup> fixture)
 
         {
             Client = fixture.Client;
+        }
+*/
+
+        public TicketapiIntegrationTest()
+        {
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost/webapp");
+            Client.DefaultRequestHeaders.Accept.Clear();
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            JObject obj = JObject.FromObject(new
+            {
+                username = "naren.7090@gmail.com",
+                password = "1234"
+            });
+
+            var body = obj.ToString();
+            HttpContent content = new StringContent(body, Encoding.UTF8, "application/json");
+            var responsetoken = Client.PostAsync("api/userapi/authenticate", content).Result;
+            string authtoken = "";
+            if (responsetoken != null)
+            {
+                var jsonString = responsetoken.Content.ReadAsStringAsync().Result;
+                JObject objrespose = JObject.Parse(jsonString);
+                authtoken = (string)objrespose["Token"];
+            }
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authtoken);
         }
 
         [Fact]
