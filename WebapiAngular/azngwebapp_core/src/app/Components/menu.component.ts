011ad5e8ed7dfbaee4daef157968afc128ba5674
Narendranath <naren.7090@outlook.com>
2020-05-10 18:48:06 +0530

    updated bootstrap to latest
    updated bootstrap to latest

--- WebapiAngular/azngwebapp_core/src/app/Components/menu.component.ts
+++ WebapiAngular/azngwebapp_core/src/app/Components/menu.component.ts
@@ -9,15 +9,16 @@ import { Router } from "@angular/router";
 @Component({
     selector: 'menu-items',
     template: `<div *ngIf='userModel'>
-    <nav class='navbar navbar-inverse' >
-        
-            <ul class='nav navbar-nav' >
-               <li  *ngFor="let cols of userModel.routeCollection"> <a [routerLink]="[cols.key]" routerLinkActive="active" > {{ cols.keyValue }} </a></li >
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse">
+            <ul class='navbar-nav mr-auto mt-2 mt-lg-0' >
+               <li  *ngFor="let cols of userModel.routeCollection" class="nav-item"> <a  class="nav-link" [routerLink]="[cols.key]" routerLinkActive="active" > {{ cols.keyValue }} </a></li >
             </ul>
-        <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> {{userModel.UserName}}</a></li>
-        <li><a href="#" (click)="logout()"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+        <ul class='navbar-nav ml-auto'>
+        <li class="nav-item"><a class="nav-link" href="#"><span class="glyphicon glyphicon-user"></span> {{userModel.UserName}}</a></li>
+        <li class="nav-item"><a class="nav-link" href="#" (click)="logout()"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
         </ul>
+        </div>
       </nav>
         </div>` ,
     // styles: ['.navbar-nav {background-color:#337AB7 !important;}']
