ace9fbe72f8cadaebba61aab19877e20187ac4eb
Narendranath <naren.7090@outlook.com>
2020-05-22 12:11:18 +0530

    Added ingeration test cases
    Added ingeration test cases

--- WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
+++ WebapiAngular/LSDataApi/LSDataApiTest/TicketapiIntegrationTest.cs
@@ -16,6 +16,8 @@ namespace LSDataApiTest
             Client = fixture.Client;
         }
 
+        #region "Get Methods"
+
         [Fact]
         public async Task GetApplicationMasterPassTest()
         {
@@ -33,5 +35,70 @@ namespace LSDataApiTest
             string responseBody = await response.Content.ReadAsStringAsync();
             Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
         }
+
+        [Fact]
+        public async Task AppMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/AppMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task RootcauseMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/RootcauseMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ModuleMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/ModuleMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task PriorityMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/PriorityMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task StatusMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/StatusMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task UserMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/UserMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TypeMasterTest()
+        {
+            var response = await Client.GetAsync("api/Ticketapi/TypeMaster");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        #endregion "Get Methods"
     }
 }
